2485f024da27f5a9772537e540e695df4299fa4a
broskoy <53642907+broskoy@users.noreply.github.com>
2026-05-18 21:21:55 +0200

    Added coreset  in __init__.py

--- src/coresets/__init__.py
+++ src/coresets/__init__.py
@@ -1,9 +1,11 @@
 from .lightweight_coreset import LightweightCoreset
 from .random_sampling import RandomSampling
 from .egb_coreset import EGBCoreset
+from .biased_coreset import BiasedCoreset
 
 ALGORITHMS = {
     "lightweight": LightweightCoreset,
     "random": RandomSampling,
     "egb": EGBCoreset,
+    "biased": BiasedCoreset
 }
